659228696abdbcdfd5e5246fe6311715f4c83fca
Dima <dima.selyutin.03@gmail.com>
2022-01-03 14:38:36 +0200

    protected from bad Internet connection

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from telebot import types
 import os
 import pyautogui as pg
 
-token = "ENTER YOUR TOKEN HERE"
+token = "ENTER_YOUR_TOKEN"
 
 bot=telebot.TeleBot(token) # creating bot
 
@@ -25,36 +25,39 @@ markup.row(volumeDown, volumeUp, volumeMute)
 
 # main function to control computer
 def echo_messages(messages):
-    for message in messages:
-        chatId = message.chat.id
-        if message.content_type == "text":
-            text = message.text
-            if text == playText:
-                pg.press("playpause")
-                bot.send_message(chatId, "\u2705")
-                bot.send_message(chatId, "\u2754", reply_markup=markup)
-            elif text.split(" ")[0] == "Block":
-                os.system("rundll32.exe user32.dll,LockWorkStation")
-                bot.send_message(chatId, "\u2705")
-                bot.send_message(chatId, "\u2754", reply_markup=markup)
-            elif text.split(" ")[0] == "Block":
-                os.system("rundll32.exe user32.dll,LockWorkStation")
-                bot.send_message(chatId, "\u2705")
-                bot.send_message(chatId, "\u2754", reply_markup=markup)
-            elif text.split(" ")[0] == "Up":
-                pg.press('volumeup', presses = 5)
-                bot.send_message(chatId, "\u2705")
-                bot.send_message(chatId, "\u2754", reply_markup=markup)
-            elif text.split(" ")[0] == "Down":
-                pg.press('volumedown', presses = 5)
-                bot.send_message(chatId, "\u2705")
-                bot.send_message(chatId, "\u2754", reply_markup=markup)
-            elif text.split(" ")[0] == "Mute":
-                pg.press('volumemute')
-                bot.send_message(chatId, "\u2705")
-                bot.send_message(chatId, "\u2754", reply_markup=markup)
-            else:
-                bot.send_message(chatId, "\u2754", reply_markup=markup)
+    try:
+        for message in messages:
+            chatId = message.chat.id
+            if message.content_type == "text":
+                text = message.text
+                if text == playText:
+                    pg.press("playpause")
+                    bot.send_message(chatId, "\u2705")
+                    bot.send_message(chatId, "\u2754", reply_markup=markup)
+                elif text.split(" ")[0] == "Block":
+                    os.system("rundll32.exe user32.dll,LockWorkStation")
+                    bot.send_message(chatId, "\u2705")
+                    bot.send_message(chatId, "\u2754", reply_markup=markup)
+                elif text.split(" ")[0] == "Block":
+                    os.system("rundll32.exe user32.dll,LockWorkStation")
+                    bot.send_message(chatId, "\u2705")
+                    bot.send_message(chatId, "\u2754", reply_markup=markup)
+                elif text.split(" ")[0] == "Up":
+                    pg.press('volumeup', presses = 5)
+                    bot.send_message(chatId, "\u2705")
+                    bot.send_message(chatId, "\u2754", reply_markup=markup)
+                elif text.split(" ")[0] == "Down":
+                    pg.press('volumedown', presses = 5)
+                    bot.send_message(chatId, "\u2705")
+                    bot.send_message(chatId, "\u2754", reply_markup=markup)
+                elif text.split(" ")[0] == "Mute":
+                    pg.press('volumemute')
+                    bot.send_message(chatId, "\u2705")
+                    bot.send_message(chatId, "\u2754", reply_markup=markup)
+                else:
+                    bot.send_message(chatId, "\u2754", reply_markup=markup)
+    except:
+        return
 
 
 bot.set_update_listener(echo_messages)
