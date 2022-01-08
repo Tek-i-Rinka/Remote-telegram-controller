aa3d5694a63bbacab8879c9e034596b4519d3eba
Oleksiy Selyutin <oselyuti@vols.utk.edu>
2022-01-07 20:41:02 -0500

    add installer shell for Terminal/Git Bash

---
+++ installer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#installer that can be ran through terminal/ Git Bash
+echo "Installing dependencies"
+echo "1.telebot - purpose ..."
+pip install telebot
+
+echo "2.pyautogui - purpose ..."
+pip install pyautogui
+
+echo "installed 2 dependencies successfully"
